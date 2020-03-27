e1aaf9e71f2037a9fbb3b10472b44781e0178efb
Grace Keane <gracekeane82@gmail.com>
2020-03-27 19:39:14 +0000

    More testing added

--- testing.py
+++ testing.py
@@ -13,3 +13,23 @@ import thompson
 # Checking does the regular expression match the string
 # Print to screen
 print(thompson.match("a.b|b*", "bbbbbbbbbbbx"))
+
+print(thompson.match("a.b|b*", "bbbbbb"))
+
+print(thompson.match("a.b|b*", "bbx"))
+
+print(thompson.match("a.b", "ab"))
+
+print(thompson.match("b**", "b"))
+
+print(thompson.match("b*", ""))
+
+print(thompson.match("A", "A"))
+
+print(thompson.match("B","B"))
+
+print(thompson.match("a.c|c*", "cccccc"))
+
+print(thompson.match("c**", "c"))
+
+print(thompson.match("a.b|c*", "dggfdert"))
